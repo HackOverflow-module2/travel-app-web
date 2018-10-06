fdae607645e0c0f3485a667a4e415c0e38308999
MartaSM <solermoran@gmail.com>
2018-10-06 17:07:41 +0200

    login

--- src/app/components/misc/login/login.component.ts
+++ src/app/components/misc/login/login.component.ts
@@ -1,4 +1,7 @@
+import { User } from './../../../shared/models/user.model';
+import { SessionService } from './../../../shared/services/session.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  user: User = new User();
 
-  constructor() { }
+  constructor(private sessionService: SessionService, private router: Router) { }
 
   ngOnInit() {
   }
 
+  onClickLogin(): void {
+    this.sessionService.authenticate(this.user)
+    .subscribe(
+    )
+  }
 }
--- src/app/shared/services/session.service.ts
+++ src/app/shared/services/session.service.ts
@@ -1,14 +1,39 @@
-import { HttpClient } from '@angular/common/http';
+import { environment } from './../../../environments/environment.prod';
+import { User } from './../models/user.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SessionService {
 
+  private static readonly SESSIONS_API = `${environment.baseApi}/sessions`;
+  private static readonly defaultOptions = {
+    headers: new HttpHeaders().set('Content-Type', 'application/json'),
+    withCredentials: true
+  };
+  private static readonly CURRENT_USER_KEY = 'current-user'
+
+  user: User = new User();
+
   constructor(private http: HttpClient) { }
 
-  authenticate() {
-
+  authenticate(user: User): Observable<User> {
+   return this.http.post<User>(SessionService.SESSIONS_API, user, SessionService.defaultOptions)
+    .pipe(
+      map((user: User) => {
+        this.doAuthenticate(user);
+        return user;
+      })
+    )
   }
+
+  private doAuthenticate(user: User): void {
+    this.user = user;
+    localStorage.setItem(SessionService.CURRENT_USER_KEY, JSON.stringify(this.user));
+  }
+
 }
