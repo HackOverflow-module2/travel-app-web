7f9632e9c582778effe65a05cdfd697b245590af
MartaSM <solermoran@gmail.com>
2018-10-06 16:29:56 +0200

    user model, login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';;
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/misc/login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/models/user.model.ts
@@ -0,0 +1,13 @@
+export class User {
+    firstName: string;
+    surname: string;
+    image?: string;
+    password?: string;
+    email: string;
+    tags?: [string];
+    rating: number;
+    role: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+    id?: string;
+}
