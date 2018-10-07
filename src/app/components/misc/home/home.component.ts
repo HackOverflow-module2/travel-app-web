e7818e49937a4a0e45b9fbcc2264793e4e6adb0a
Georgina <paceygeorgina@gmail.com>
2018-10-07 12:57:01 +0200

    add less

--- src/app/components/misc/home/home.component.ts
+++ src/app/components/misc/home/home.component.ts
@@ -3,10 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.less']
 })
 export class HomeComponent implements OnInit {
-  images = [1, 2, 3].map(() => `../assets/images/home/home-slide-${Math.floor(Math.random() * 5)}.jpg`);
 
     constructor() { }
 
