169830f8d25aca9622c9c9d8987521fd45933be1
Georgina <paceygeorgina@gmail.com>
2018-10-12 11:08:35 +0200

    remove poi of interest map

--- src/app/components/pois/list/list.component.ts
+++ src/app/components/pois/list/list.component.ts
@@ -14,20 +14,12 @@ export class ListComponent implements OnInit {
   origin: Coordinates = this.mapService.getOrigin();
   destination: Coordinates = this.mapService.getDestination();
   pois: Array<Poi> = [];
-  coordinates: Array<Coordinates> = [];
 
   constructor(private mapService: MapService, private poiService: PoiService) {   }
 
   ngOnInit() {
     this.poiService.list().subscribe((pois: Array<Poi>) => {
       this.pois = pois;
-    });
-  }
-
-  getPoisCoordinates() {
-    this.pois.map(poi => {
-     const coordinate: Coordinates = {lat: poi.location.coordinates[0], lng: poi.location.coordinates[1]};
-     this.coordinates.push(coordinate);
     });
   }
 
