ada58dad874f66f9d5472f5d4ca2b39d55917980
MartaSM <solermoran@gmail.com>
2018-10-15 14:17:23 +0200

    Trip detail

--- src/app/components/trips/trip-detail/trip-detail.component.ts
+++ src/app/components/trips/trip-detail/trip-detail.component.ts
@@ -1,4 +1,8 @@
+import { map, switchMap } from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
+import { TripService } from './../../../shared/services/trip.service';
 import { Component, OnInit } from '@angular/core';
+import { Trip } from '../../../shared/models/trip.model';
 
 @Component({
   selector: 'app-trip-detail',
@@ -7,9 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TripDetailComponent implements OnInit {
 
-  constructor() { }
+  trip: Trip = new Trip();
+  tripId: string;
+
+  constructor(private tripService: TripService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.pipe(
+      map(params => this.tripId = params.id),
+      switchMap((tripId: string) => this.tripService.get(tripId))
+    ).subscribe((trip: Trip) => this.trip = trip)
   }
+
 
 }
