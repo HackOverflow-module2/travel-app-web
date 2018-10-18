c1f1cd3cc409ad9108da4d5a6c068277a8831f64
Georgina <paceygeorgina@gmail.com>
2018-10-18 21:57:30 +0200

    styles

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
   title: String = 'My first AGM project';
-  marta: Number = 40.4168;
+  lat: Number = 40.4168;
   lng: Number = 3.7038;
 
   constructor() { }
