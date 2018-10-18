0caed96c45075bec294dd9dbfe988f5b1337dc5a
MartaSM <solermoran@gmail.com>
2018-10-18 19:52:54 +0200

    a medias

--- src/app/components/trips/trip-form/trip-form.component.ts
+++ src/app/components/trips/trip-form/trip-form.component.ts
@@ -1,3 +1,6 @@
+import { TripCreateComponent } from './../trip-create/trip-create.component';
+import { Router } from '@angular/router';
+import { TripService } from './../../../shared/services/trip.service';
 import { Coordinates } from './../../../shared/models/coordinates.model';
 import { ApiError } from './../../../shared/models/api-error.model';
 import { MapService } from './../../../shared/services/map.service';
@@ -15,21 +18,28 @@ export class TripFormComponent implements OnInit {
 
   @Input() tripPois: Array<Poi> =[];
   @Input() trip: Trip = new Trip();
-  @Input() inTripEdition: boolean = false;
+  tripId: string;
   @Input() origin: Coordinates;
   @Input() destination: Coordinates;
   @ViewChild('tripForm') tripForm: FormGroup;
-  @Output() tripSubmit: EventEmitter<Trip> = new EventEmitter();
+
   apiError: ApiError;
+  typeOfFunctionality: {
+    
+  }
+  
+  
+ 
+ 
 
 
-  constructor(private mapService: MapService) { }
+  constructor(private mapService: MapService, private router: Router, private tripService: TripService) { }
 
   ngOnInit() {
-    this.trip.originLocation[0] = this.mapService.getOrigin();
-    this.trip.originLocation[1] = this.mapService.getOrigin();
-    this.trip.destinationLocation[0] = this.mapService.getDestination();
-    this.trip.destinationLocation[1] = this.mapService.getDestination();
+    this.trip.originLocation[0] = this.mapService.origin.lat;
+    this.trip.originLocation[1] = this.mapService.origin.lng;
+    this.trip.destinationLocation[0] = this.mapService.destination.lat;
+    this.trip.destinationLocation[1] = this.mapService.destination.lng;
   }
   
   onChangeGalleryFile(image: HTMLInputElement): void {
@@ -41,20 +51,23 @@ export class TripFormComponent implements OnInit {
     }
   }
 
-  onSubmitTripForm():void {
+  onSubmitTripCreate():void {
     if(this.tripForm.valid) {
-      if(this.inTripEdition) {
-        this.trip.originLocation[0] = this.origin.lat;
-        this.trip.originLocation[1] = this.origin.lng;
-        this.trip.destinationLocation[0] = this.destination.lat;
-        this.trip.destinationLocation[1] = this.destination.lng;
-        this.inTripEdition = false;
-      }
-        const tripPoiId = this.tripPois.map(tripPoi => tripPoi.id);
-        this.trip.pois = tripPoiId;
-        this.tripSubmit.emit(this.trip);
+        this.tripService.create(this.trip)
+        .subscribe((trip: Trip) => {
+      this.router.navigate(['trips', trip.id]);
+    })
+      
     }
   }
+
+  switcher(item): {
+    this.items[item]
+  }
+
+  this.create = item => this.tripService.create(this.trip)
+  this.edit = item => this.tripService.edit(this.tripId, this.trip)
+
 
 
 
